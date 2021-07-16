1707cdb50594fd54c6ff14fb53aab16a2bdcac5c
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-07-16 12:06:23 +0530

    modified: empWage attendence feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,10 @@
 #! /bin/bash -x
-
+#constants
+IS_PRESENT=1
 echo "welcome to Employeewage program"
+if [ $empcheck -eq $IS_PRESENT ]
+then
+echo "Employee is present"
+else
+echo "Employee is absent"
+fi
